7dddc9c07987e619355678b38c4848e21e3299dd
Hikawa Rin <HikawaRin@outlook.com>
2022-12-26 20:07:27 +0800

    Complex TSX

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,42 @@ class DiceInstance implements IDice {
   getValue(): number {
     return this.value_;
   }
+
+  roll(): void {
+    this.value_ = Math.floor(Math.random() * 6) + 1;
+  }
 }
 
 function App() {
-  let dice: DiceInstance = new DiceInstance();
-  let desc = 'Ro';
+  let dice_num: number = 10;
+  let dices: DiceInstance[] = [];
+  for (let i = 0; i < dice_num; i++) {
+    dices.push(new DiceInstance());
+  }
+  
   return (
     <main>    
       <h1>Tenzies</h1>
       <div className='tenzies-describe'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</div>
       <div className='dice-container'>
-        <Dice instance={dice} />
+        {
+          dices.map((dice, index) => {
+            return (
+              <Dice key={index} instance={dice} />
+            );
+          })
+        }
       </div>
-      <button className='tenzies-roll'>
-        { `${desc}ll` }
+      <button 
+        className='tenzies-roll'
+        onClick={() => {
+          for (let i = 0; i < dice_num; i++) {
+            dices[i].roll();
+            console.log(dices[i].getValue());
+          }
+        }}
+      >
+        Roll
       </button>
     </main>
   );
