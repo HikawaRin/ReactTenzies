3c17939356a5e060b3019a9d86f23ca7bbe5470a
Hikawa Rin <HikawaRin@outlook.com>
2022-12-26 19:44:47 +0800

    Basic TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dice, { IDice } from './Dice';
+import "./App.css";
+
+class DiceInstance implements IDice { 
+  private value_: number;
+  constructor() {
+    this.value_ = 0;
+  }
+
+  getValue(): number {
+    return this.value_;
+  }
+}
 
 function App() {
+  let dice: DiceInstance = new DiceInstance();
+  let desc = 'Ro';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>    
+      <h1>Tenzies</h1>
+      <div className='tenzies-describe'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</div>
+      <div className='dice-container'>
+        <Dice instance={dice} />
+      </div>
+      <button className='tenzies-roll'>
+        { `${desc}ll` }
+      </button>
+    </main>
   );
 }
 
