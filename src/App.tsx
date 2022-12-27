c2bd56b34af37c10734ad07c0d235b19d52c09a4
Hikawa Rin <HikawaRin@outlook.com>
2022-12-27 11:20:45 +0800

    more State

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import React  from 'react';
 import "./App.css";
 
 class DiceInstance implements IDice { 
+  private id_: number;
   private value_: number;
-  constructor() {
+  private selected_: boolean;
+  constructor(id: number) {
+    this.id_ = id;
     this.value_ = 0;
+    this.selected_ = false;
   }
 
-  getValue(): number {
-    return this.value_;
-  }
+  getId() { return this.id_; }
+  getValue(): number { return this.value_; }
+  isSelected(): boolean { return this.selected_; }
+  touch(selected: boolean): void { this.selected_ = selected; } 
 
   roll(): void {
     this.value_ = Math.floor(Math.random() * 6) + 1;
@@ -19,39 +24,80 @@ class DiceInstance implements IDice {
 
 function App() {
   let dice_num: number = 10;
-  let dices_: DiceInstance[] = [];
-  for (let i = 0; i < dice_num; i++) {
-    dices_.push(new DiceInstance());
-  }
-  const [dices, setDices] 
-    = React.useState<DiceInstance[]>(dices_);
   
+  const [dices, setDices] = React.useState<DiceInstance[]>(() => {
+    let dices: DiceInstance[] = [];
+    for (let i = 0; i < dice_num; i++) {
+      dices.push(new DiceInstance(i));
+      dices[i].roll();
+    }
+    return dices; 
+  });
+  const [round, setRound] = React.useState<number>(0);
+  const [GameOver, setGameOver] = React.useState<boolean>(false);
+
+  let SetDice = (id: number, selected: boolean) => {
+    setDices((prevDices) => {
+      return prevDices.map((dice) => {
+        if (dice.getId() === id) {
+          dice.touch(selected);
+        }
+        return dice;
+      });
+    });
+  }
+
+  let RollDice = () => {
+    setDices((prevDices) => {
+      return prevDices.map((dice) => {
+        if (!dice.isSelected()) {
+          dice.roll();
+        }
+        return dice;
+      });
+    });
+  }
+
+  let NewGame = () => {
+    setDices((prevDices) => {
+      return prevDices.map((dice) => {
+        dice.touch(false);
+        return dice;
+      });
+    });
+    setRound(0);
+    setGameOver(false);
+  }
+
   return (
     <main>    
       <h1>Tenzies</h1>
       <div className='tenzies-describe'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</div>
+      <div className='tenzies-describe'>{`You have rolled ${round} Round`}</div>
       <div className='dice-container'>
         {
           dices.map((dice, index) => {
             return (
-              <Dice key={index} instance={dice} />
+              <Dice 
+                key={index} 
+                instance={dice} 
+                setDice={SetDice} 
+              />
             );
           })
         }
       </div>
       <button 
         className='tenzies-roll'
-        onClick={() => {
-          setDices((prevDices) => {
-            prevDices.forEach((dice) => {
-              dice.roll();
-              console.log(dice.getValue());
-            });
-            return [...prevDices];
-          })
+        onClick={(event) => { 
+          setRound((prevRound) => prevRound + 1);
+          if (GameOver) {
+            NewGame();
+          } 
+          RollDice(); 
         }}
       >
-        Roll
+        { GameOver ? 'New Game' : 'Roll' }
       </button>
     </main>
   );
--- src/Dice.tsx
+++ src/Dice.tsx
@@ -1,16 +1,23 @@
 import "./Dice.css";
 
 export interface IDice {
+  getId(): number;
   getValue(): number;
+  isSelected(): boolean;
 }
 
 interface Wrapper_{
   instance: IDice;
+  setDice: (id: number, selected: boolean) => void;
 }
 
-function Dice({ instance }: Wrapper_) {
+function Dice({ instance, setDice }: Wrapper_) {
+  let style = instance.isSelected() ? 'selected' : '';
   return (
-    <div className='dice'>
+    <div 
+      className={`dice ${style}`}
+      onClick={(event) => setDice(instance.getId(), !instance.isSelected())}
+    >
       <p>{ instance.getValue() }</p>
     </div>
   );
